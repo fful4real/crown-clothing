00a3ca47bd408eb728c101a415a7f5ad4efec309
Felix FUL CHI <fful4real@yahoo.com>
2019-12-15 15:55:02 +0400

    Cloned from Yihua

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,7 +13,6 @@ const middlewares = [sagaMiddleware];
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
-
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 sagaMiddleware.run(rootSaga);
